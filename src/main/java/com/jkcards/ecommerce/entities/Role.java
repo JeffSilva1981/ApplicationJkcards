43f78aaaa888d5657d3bbe854ef17ffb269c34f1
JeffSilva1981 <jeff.contactwork@gmail.com>
2025-11-02 14:22:13 -0300

    Implements User, Role with Spring Security and BCrypt

---
+++ src/main/java/com/jkcards/ecommerce/entities/Role.java
@@ -0,0 +1,48 @@
+package com.jkcards.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String authority;
+
+    public Role(long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user(first_name, last_name, email, password) VALUES ('Vitor', 'Luan', 'vitorluan@gmail.com', '$2a$10$4GCdS85pkpDRVbga.FAWSuary8O3lcATobPoQ0nh56m85N8sTCJpy');
+INSERT INTO tb_user(first_name, last_name, email, password) VALUES ('Jeferson', 'Ferreira da silva', 'jfds.ferreirasilva@gmail.com', '$2a$10$4GCdS85pkpDRVbga.FAWSuary8O3lcATobPoQ0nh56m85N8sTCJpy');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
